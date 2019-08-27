6d3a4a573443cf43c937b3dcd2dec36ee7c91184
viky28 <viky.pu28@gmail.com>
2019-08-27 14:02:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl, FormArray, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dynamicform';
+  userForm:FormGroup
+  get name(){
+  	return this.userForm.get('name');
+  }
+  constructor(){}
+
+
+  ngOnInit(){
+  	this.userForm = new FormGroup({
+  		name:new FormControl('ram'),
+  		age : new FormControl('29'),
+  		hobbies: new FormArray([])
+  	});
+  }
+  addHobby(){
+  	(<FormArray>this.userForm.get('hobbies')).push(new FormControl(""));
+  }
+  submit() {
+  	console.log("submit data ",this.userForm)
+  }
+
 }
